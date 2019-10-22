8599d373f1088006aef41b140743f0cf149bf778
lebmunoz <le.bmunoz@gmail.com>
2019-10-22 20:30:32 -0300

    dev request handling

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import { Alert, SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
 import logo from '../assets/logo.png'
 import SpotList from '../components/SpotList'
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user_id => {
+      const socket = socketio('http://192.168.1.110:3333', {
+        query: { user_id }
+      })
+
+      socket.on('booking_response', booking => {
+        Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA': 'REJEITADA'}`)
+      })
+    })
+  }, []);
 
   // [ReactJS, [ Node.js]]
 
