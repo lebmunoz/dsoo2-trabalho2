5684e8b759c96c6e79c912095960ed8b637bbbcd
Luis Enrique <luis.munoz@bridge.ufsc.br>
2020-03-09 22:57:11 -0300

    wemos first code

--- wemos/wemos.ino
+++ wemos/wemos.ino
@@ -1,9 +1,27 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server;
+
+char* ssid = "Teste";
+char* password = "test1234";
 
 void setup() {
+    WiFi.hostname("ESP-host");
+    WiFi.begin(ssid,password);
+    Serial.begin(115200);
+    while(WiFi.status()!=WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+    Serial.println("");
+    Serial.print("IP Address: ");
+    Serial.print(WiFi.localIP());
 
+    server.on("/",[](){server.send(200,"text/plain","Hellou bridge!");});
+    server.begin();
 }
 
 void loop() {
-
+    server.handleClient();
 }
