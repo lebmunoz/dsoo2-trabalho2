8cfeee05d361efbef2f209b19b5695987e273c4a
Luis Enrique <luis.munoz@bridge.ufsc.br>
2020-03-11 20:55:26 -0300

    add toggle route

--- wemos/wemos.ino
+++ wemos/wemos.ino
@@ -2,11 +2,12 @@
 #include <ESP8266WebServer.h>
 
 ESP8266WebServer server;
-
+uint8_t pin_led = 2;
 char* ssid = "Teste";
 char* password = "test1234";
 
 void setup() {
+    pinMode(pin_led, OUTPUT);
     WiFi.hostname("ESP-host");
     WiFi.begin(ssid,password);
     Serial.begin(115200);
@@ -19,9 +20,16 @@ void setup() {
     Serial.print(WiFi.localIP());
 
     server.on("/",[](){server.send(200,"text/plain","Hellou bridge!");});
+    server.on("/toggle",toggleLED);
     server.begin();
 }
 
 void loop() {
     server.handleClient();
+}
+
+void toggleLED() {
+    digitalWrite(pin_led,!digitalRead(pin_led));
+    delay(500);
+    server.send(204,"");
 }
